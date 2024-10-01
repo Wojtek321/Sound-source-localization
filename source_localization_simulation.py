2a3dbeebecac844f56cd61307653a010cfe318e3
Wojciech Domański <wojtekdxd@gmail.com>
2024-10-01 20:52:22 +0200

    Add audio recording from microphone and file uploading

--- source_localization_simulation.py
+++ source_localization_simulation.py
@@ -25,6 +25,10 @@ SIGNALS = {
     'Trumpet': 'trumpet.wav',
 }
 
+scaler_itd = load(os.path.join(MODELS_PATH, 'scaler_itd.joblib'))
+scaler_ild = load(os.path.join(MODELS_PATH, 'scaler_ild.joblib'))
+scaler_y = load(os.path.join(MODELS_PATH, 'scaler_y.joblib'))
+
 sofa_file = Dataset(os.path.join(SOFA_PATH, 'sadie.sofa'))
 SOFA_FS = sofa_file['Data.SamplingRate'][:][0]
 ARR = sofa_file['SourcePosition'][:]
@@ -32,13 +36,15 @@ ARR = sofa_file['SourcePosition'][:]
 TIME = 1
 
 
-def pred(angle, sound_id, model_id):
-    model = load(os.path.join(MODELS_PATH, MODELS[model_id]))
-    scaler_itd = load(os.path.join(MODELS_PATH, 'scaler_itd.joblib'))
-    scaler_ild = load(os.path.join(MODELS_PATH, 'scaler_ild.joblib'))
-    scaler_y = load(os.path.join(MODELS_PATH, 'scaler_y.joblib'))
+def pred(sound_id, mic_input, angle, model_id):
+    if mic_input is None:
+        fs, data = wavfile.read(os.path.join(SIGNALS_PATH, SIGNALS[sound_id]))
+    else:
+        fs, data = mic_input
 
-    fs, data = wavfile.read(os.path.join(SIGNALS_PATH, SIGNALS[sound_id]))
+    if data.ndim == 2:
+        data = data.sum(axis=-1) / data.shape[1]
+
     data = data / (np.max(np.abs(data)))
     data = resample(data, orig_sr=fs, target_sr=SOFA_FS)
 
@@ -61,6 +67,8 @@ def pred(angle, sound_id, model_id):
     itd = scaler_itd.transform(np.reshape(itd, (-1, 1)))
     ild = scaler_ild.transform(np.reshape(ild, (-1, 1)))
 
+    model = load(os.path.join(MODELS_PATH, MODELS[model_id]))
+
     X = np.column_stack((itd, ild))
     Y = model.predict(X)
     Y = scaler_y.inverse_transform(np.reshape(Y, (-1, 1)))
@@ -74,8 +82,9 @@ demo = gr.Interface(
     title='Simulating spatial sound and determining its source direction.',
     fn=pred,
     inputs=[
-        gr.Slider(minimum=-90, maximum=90, value=0, step=5, label='Azimuth angle', info='Angle equal to 0 means a sound from the opposite side, closer to -90 is a sound from the left, and closer to 90 from the right.'),
         gr.Dropdown(choices=[key for key, _ in SIGNALS.items()], value='Eye of the tiger', multiselect=False, label='Sound to be simulated'),
+        gr.Audio(sources=['upload', 'microphone'], type='numpy', label="Record sound or upload .wav file"),
+        gr.Slider(minimum=-90, maximum=90, value=0, step=5, label='Azimuth angle', info='Angle equal to 0 means a sound from the opposite side, closer to -90 is a sound from the left, and closer to 90 from the right.'),
         gr.Dropdown(choices=[key for key, _ in MODELS.items()], value='Decision Tree', multiselect=False, label='Model to be used'),
     ],
     outputs=[
